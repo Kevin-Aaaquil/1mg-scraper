e04781ff25b8532fc83e6561d534218ef53f0136
Kevin-Aaaquil <eshansingh2409@gmail.com>
2022-09-01 14:16:46 +0530

    run on server

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import axios from "axios";
+import cheerio from "cheerio";
+import * as fs from "fs";
+
+const fetchListOfSalts = async () => {
+    try{
+        console.log("Fetching list of salts...");
+        const salts = []
+        let id =1
+        for(let i=1;i<=5;i++){
+            const url = "https://www.1mg.com/drug-ailments/---248?pageNumber="+i;
+            console.log("Fetching page " + url);
+            const response = await axios.get(url);
+            const html = response.data;
+            const $ = cheerio.load(html);
+
+            $('div.ListPage__breakWord__qF_cj.col-3.marginTop-8').each((i, el) => {
+                const salt = $(el)
+                const saltName = salt.find('a').text();
+                const saltUrl = "https://www.1mg.com"+salt.find('a').attr('href');
+                salts.push({id, saltName,saltUrl})
+                id++
+            })
+        }
+        return salts
+    }
+    catch(err){
+        console.log(err);
+        throw(err);
+    }
+}
+
+
+const fetchSaltMedicines = async (salts) => {
+try {
+    const saltMedicines = []
+    let id = 1
+    for(let i=0;i<salts.length;i++){
+        const salt  = salts[i];
+        const url = salt.saltUrl;
+        console.log("Fetching page " + url);
+        const response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
+        $('a.noAnchorColor').each((i, el) => {
+            const data = $(el)
+            const medicine = {
+                id: id++,
+                saltName: salt.saltName,
+                medicineName : data.find('div.flex.paddingBoth-8').find('div.flexColumn.col-3.SingleAttributeComparison__wrap__Va2gi').find('div.bodySemiBold').find('div').text,
+                manufacturer : data.find('div.flex.paddingBoth-8').find('div.flexColumn.col-3.SingleAttributeComparison__wrap__Va2gi').find('div.smallRegular.SingleAttributeComparison__subtitle__TRmce').text
+            }
+            saltMedicines.push(medicine)
+
+        })
+    }
+    return saltMedicines
+} catch (error) {
+    console.log(error);
+    throw error;
+}
+    
+}
+
+
+(async ()=>{
+const data = await fetchListOfSalts();
+const saltMedicines = await fetchSaltMedicines(data);
+const json = JSON.stringify(saltMedicines);
+fs.writeFileSync('saltMedicines.json', json);
+// await fetchSaltMedicines()
+})()
+
+
+
+
+
